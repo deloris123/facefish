c19e8603c32e8ea14ac16863904928f2bdfd6677
emerueij <ikennaemerue13@gmail.com>
2025-11-29 00:14:24 +0100

    first deploy

--- facefish/urls.py
+++ facefish/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from facefishapp import views
+from django.http import HttpResponseRedirect
+
 
 urlpatterns = [
     # This instantly redirects Render's initialization page to your real Facebook page
